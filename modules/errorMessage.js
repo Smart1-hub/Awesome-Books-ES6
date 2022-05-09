051cc78a872cde5bc10bcdc3de298dd2432ef17c
Ademola Adebayo <scdk@yahoo.com>
2022-05-09 15:17:40 +0100

    Review html

--- modules/errorMessage.js
+++
@@ -1,6 +0,0 @@
-export default (error) => {
-  document.querySelector('.error-msg').innerHTML = error;
-  setTimeout(() => {
-    document.querySelector('.error-msg').innerHTML = '';
-  }, 2000);
-};
